d9ebd21883a2825ec7d93abaa42c6b24eea37ae3
Fanjunhong0612 <64936437+Fanjunhong0612@users.noreply.github.com>
2020-05-07 09:20:40 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(Sting args[]){
-		System.out.println("Hello Fanyukun!");
+		System.out.println("Hello Fanyukun LOVE ME!");
 		System.out.println("Hello World!");
 	}
 }
